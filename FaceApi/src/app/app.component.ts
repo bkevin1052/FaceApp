a5173a1c4bda5be26bd53cbdd0568d7293d4a768
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-10-27 00:58:52 -0600

    agregando camara

--- FaceApi/src/app/app.component.ts
+++ FaceApi/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { AzureCognitiveServicesService } from './services/azure-cognitive-services.service';
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +13,69 @@ export class AppComponent {
 
   ImageInfo: string;
 
+  WIDTH = 640;
+  HEIGHT = 480;
+
+  @ViewChild("video")
+  public video: ElementRef;
+
+  @ViewChild("canvas")
+  public canvas: ElementRef;
+
+  captures: string[] = [];
+  error: any;
+  isCaptured: boolean;
+
+  async ngAfterViewInit() {
+    await this.setupDevices();
+  }
+
+  async setupDevices() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true
+        });
+        if (stream) {
+          this.video.nativeElement.srcObject = stream;
+          this.video.nativeElement.play();
+          this.error = null;
+        } else {
+          this.error = "You have no output video device";
+        }
+      } catch (e) {
+        this.error = e;
+      }
+    }
+  }
+
+  capture() {
+    this.drawImageToCanvas(this.video.nativeElement);
+    this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+    this.isCaptured = true;
+  }
+
+  removeCurrent() {
+    this.isCaptured = false;
+  }
+
+  setPhoto(idx: number) {
+    this.isCaptured = true;
+    var image = new Image();
+    image.src = this.captures[idx];
+    this.drawImageToCanvas(image);
+  }
+
+  drawImageToCanvas(image: any) {
+    this.canvas.nativeElement
+      .getContext("2d")
+      .drawImage(image, 0, 0, this.WIDTH, this.HEIGHT);
+  }
+
   constructor(private data:AzureCognitiveServicesService){
     this.imageUrl = '';
     this.ImageInfo = '';
+    this.captures = [];;
   }
 
   getInfo(imageUrl:string){
@@ -24,7 +84,5 @@ export class AppComponent {
         this.ImageInfo = data;
       })
   }
-
-
 
 }
