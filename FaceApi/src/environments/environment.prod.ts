b064af04ddd2b2191051b4dacb9d3213bc7452f3
Eduardo Peláez <eapelaezc@correo.url.edu.gt>
2021-11-01 18:38:13 -0600

    environment url

--- FaceApi/src/environments/environment.prod.ts
+++ FaceApi/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseURL: "https://url-somosfamiliares-gt.cognitiveservices.azure.com/",
-  server: "https://url-somosfamiliares-gt.cognitiveservices.azure.com/face/v1.0/detect?detectionModel=detection_03&returnFaceId=true&returnFaceLandmarks=false"
+  server: "https://url-somosfamiliares-gt.cognitiveservices.azure.com/face/v1.0/detect?detectionModel=detection_03&returnFaceId=true&returnFaceLandmarks=false",
+  login: "https://faceapuazure.herokuapp.com",
+  baseURL: "https://url-somosfamiliares-gt.cognitiveservices.azure.com/"
 };
